1ecca66fa2cb36e3f8b7d01811a21b943633c43f
kartik Aiyar <kartik.aiyar82@gmail.com>
2018-10-24 20:43:43 +0000

    Added friend profile tracking

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: "users#my_friends"
   resources :user_stocks, only:[:create, :destroy]
+  resources :users, only:[:show]
+  resources :friendships
+  get "search_friends", to: "users#search"
+  post "add_friend", to: "users#add_friend"
   
 
   # Example of regular route:
